5c6b85273bf8420f4dad5f269138ccc8c2f0ce0b
Chris Yoon <chris.yoon@globalrelay.net>
2013-08-29 17:34:16 -0700

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+	before_save {self.email = email.downcase}
+	validates :email, presence: true, uniqueness: true
+	validates :password, presence: true, length: {minimum: 6}
+
 end
