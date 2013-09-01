2cf1d1d45b61d4f18902ae1a8d14ba19f7e20de5
Chris <chris.yoon90@gmail.com>
2013-09-01 00:37:57 -0700

    finished simple Auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,16 +4,18 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		user = User.find(params[:session][:email].downcase)
-		if user && user.authenticate(params[:session][:password])
+		user = User.find_by(email: params[:email].downcase)
+		if user && user.authenticate(params[:password])
 			session[:user_id] = user.id
 			redirect_to root_path, notice: "Logged in!"
 		else
+			flash.now[:error] = "Invalid email or password"
 			render 'new'
 		end
 	end
 
 	def destroy
-
+		session[:user_id] = nil
+		redirect_to root_path, notice: "Logged out!"
 	end
 end
